d89ec12ebb05038ef4c39950de2b66b80791431c
davemccann <davemccann@users.noreply.github.com>
2025-01-22 22:35:05 +0000

    Added tool caching support
    Removed premake execution now only performs setup

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
-const toolchain = require('./toolchain');
 const path = require('path');
-const RunnerConfig = require('./runner-config');
-const { getPlatformString, getPlatformArchiveExt } = require('./platform');
+const { v4: uuidv4 } = require('uuid');
+const toolchain = require('./toolchain');
+const { generatePremakeURL, getPlatformArchiveExt } = require('./platform');
 
 async function main() {
     try {
-        cfg = RunnerConfig.create(
-            core.getInput('version', { required: true }),
-            core.getInput('action', { required: true }),
-            core.getInput('options', { required: false }),
-            core.getInput('installPath', { required: false }),
-            core.getInput('premakeFilepath', { required: false })
-        );
-        cfg.printRunnerConfig();
+        cfg = {
+            version: core.getInput('version', { required: true }),
+            toolname: 'premake',
+        };
 
-        const platform = getPlatformString();
+        // Search the cache for the version and use that if exists
+        cachedPath = tc.find(cfg.toolname, cfg.version);
+        if (cachedPath) {
+            core.addPath(cachedPath);
+            process.exit(0);
+        }
+
+        const premakeTempPath = path.join(process.env.RUNNER_TEMP, cfg.toolname);
+
+        // Attempt to download binary with the version specified 
+        const premakeURL = generatePremakeURL(cfg.version);
+        const downloadPath = path.join(premakeTempPath, uuidv4());
+        const premakePath = await tc.downloadTool(premakeURL, downloadPath);
+
+        // Extract the executable from the archive to temp storage
         const archiveExt = getPlatformArchiveExt();
+        const extractFolder = await toolchain.extractArchive(premakePath, premakeTempPath, archiveExt);
 
-        const baseURL = `https://github.com/premake/premake-core/releases/download`;
-        const premakeURL = path.join(baseURL, `v${cfg.version}/premake-${cfg.version}-${platform}.${archiveExt}`);
-
-        // Add this path to the environment for this run
-        core.addPath(cfg.installPath);
-
-        // Attempt to download binary with the version specified
-        console.log("test");
-        const premakePath = await tc.downloadTool(premakeURL);
-        console.log(`premake path: ${premakePath}`);
-
-        // Extract the executable from the archive
-        const extractFolder = await toolchain.extractArchive(premakePath, cfg.installPath, archiveExt);
-
-        // Execute premake and pass in the specified arguments
-        var args = [cfg.action];
-        if (cfg.premakeFilepath !== '') {
-            args.push(`--file=${cfg.premakeFilepath}`);
-        }
-        if (cfg.options !== '') {
-            args.push(cfg.options);
-        }
-
-        console.log(`run ${extractFolder}/premake5 ${args}`);
-        toolchain.execApp(`${extractFolder}/premake5`, args);
+        // Cache the executable by version for next run
+        const targetFilename = process.platform === 'win32' ? 'premake5.exe' : 'premake5';
+        cachedPath = await tc.cacheFile(
+            path.join(extractFolder, targetFilename),
+            targetFilename,
+            cfg.toolname,
+            cfg.version
+        );
+        core.addPath(cachedPath);
 
     } catch (error) {
         console.error(`action failed: ${error.message}`);
